268504c74632b2b1a61f92095727d22bdf7d17e7
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2026-02-24 23:49:59 +0000

    Added Temp References for building app [remove after completed]

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AlertProvider } from "./context/alertContext";
 
+
+/* TEMP References
+* Chakra-UI: https://chakra-ui.com/docs/components/concepts/overview
+* Formik: https://formik.org/docs/overview
+* Yuk: https://github.com/jquense/yup
+*/
+
 function App() {
   return (
     <ChakraProvider>
       <AlertProvider>
         <main>
-          
+
         </main>
       </AlertProvider>
     </ChakraProvider>
