d54d8f01ac2b9c35e56ef321d40996dafb1eb7b4
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2026-03-07 18:54:28 +0000

    Had to revier changes removing the non-existent @charkara-ui/avatar package.

--- portfolio/src/components/LandingSection.js
+++ portfolio/src/components/LandingSection.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Avatar } from "@chakra-ui/avatar";
-import { Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const greeting = "Hello, I am Pete!";
@@ -9,20 +8,22 @@ const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
-const LandingSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
-  >
-    <VStack spacing={4}>
-      <Avatar src="https://i.pravatar.cc/150?img=7" size="2xl" />
-      <Heading as="h1" size="xl">{greeting}</Heading>
-      <Heading as="h2" size="lg">{bio1}</Heading>
-      <Heading as="h3" size="md">{bio2}</Heading>
-    </VStack>
-  </FullScreenSection>
-);
+function LandingSection() {
+  return (
+    <FullScreenSection
+      justifyContent="center"
+      alignItems="center"
+      isDarkBackground
+      backgroundColor="#2A4365"
+    >
+      <VStack spacing={4}>
+        <Avatar src="https://i.pravatar.cc/150?img=7" size="2xl" />
+        <Heading as="h1" size="xl">{greeting}</Heading>
+        <Heading as="h2" size="lg">{bio1}</Heading>
+        <Heading as="h3" size="md">{bio2}</Heading>
+      </VStack>
+    </FullScreenSection>
+  );
+}
 
 export default LandingSection;
